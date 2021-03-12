7d3b6df5f4ee68b9b68ef6b70b1e8f1874a634fe
Kryukov.SA <sergey.krykov.2806@gmail.com>
2021-03-12 09:30:34 +0300

    start

---
+++ modules/task_1/kryukov_s_sparse_compl_mat/main.cpp
@@ -0,0 +1,14 @@
+// Copyright 2021 Kryukov Sergey
+#include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
+#include "./kryukov_s_sparse_compl_mat.h"
+
+
+
+
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
