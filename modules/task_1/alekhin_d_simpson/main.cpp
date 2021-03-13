7916418f0284bd79868659ae11c510cedaab25d3
Denis <alehin.de@gmail.com>
2021-03-13 08:09:26 +0300

    Initial commit

---
+++ modules/task_1/alekhin_d_simpson/main.cpp
@@ -0,0 +1,7 @@
+// Copyright 2021 Alekhin Denis
+#include <gtest/gtest.h>
+#include "../../modules/task_1/alekhin_d_simpson/simpson.h"
+
+TEST(First_Test, test) {
+  EXPECT_EQ(0, 0);
+}
---
+++ modules/task_1/alekhin_d_simpson/simpson.cpp
@@ -0,0 +1,3 @@
+// Copyright 2021 Alekhin Denis
+
+#include "../../modules/task_1/alekhin_d_simpson/simpson.h"
