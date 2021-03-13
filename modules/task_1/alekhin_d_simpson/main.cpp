926699d9cca4114de4e1662fd872b5c9d0e2783f
Denis <alehin.de@gmail.com>
2021-03-13 11:04:17 +0300

    Bug need to be fixed

--- modules/task_1/alekhin_d_simpson/main.cpp
+++ modules/task_1/alekhin_d_simpson/main.cpp
@@ -2,6 +2,96 @@
 #include <gtest/gtest.h>
 #include "../../modules/task_1/alekhin_d_simpson/simpson.h"
 
-TEST(First_Test, test) {
-  EXPECT_EQ(0, 0);
+// y = x1( * x2 * ...)
+std::function<double(std::vector<double>)> f1 = [](std::vector<double> arg) {
+  double result = 1;
+  for (size_t i = 0; i < arg.size(); i++) {
+    result *= arg[i];
+  }
+  return result;
+};
+
+// y = x1( + x2 + ...)
+std::function<double(std::vector<double>)> f2 = [](std::vector<double> arg) {
+  double result = 0;
+  for (size_t i = 0; i < arg.size(); i++) {
+    result += arg[i];
+  }
+  return result;
+};
+
+TEST(Functions_Test, Function_F1_Test1) {
+  std::vector<double> arg = { 1 };
+
+  EXPECT_DOUBLE_EQ(1.0, f1(arg));
+}
+
+TEST(Functions_Test, Function_F1_Test2) {
+  std::vector<double> arg = { 5, 20 };
+
+  EXPECT_DOUBLE_EQ(100.0, f1(arg));
+}
+
+TEST(Functions_Test, Function_F1_Test3) {
+  std::vector<double> arg = { 1, 2, -1, 20, 0.001, 20 };
+
+  EXPECT_DOUBLE_EQ(-0.8, f1(arg));
+}
+
+TEST(Input_Data_Test, Scope_Interval_Test1) {
+  std::vector<std::pair<double, double>> scope =
+  { std::make_pair<double, double>(1, 0) };
+  
+  EXPECT_ANY_THROW(simpsonMethod(scope, f1, 10));
+}
+
+TEST(Input_Data_Test, Scope_Interval_Test2) {
+  std::vector<std::pair<double, double>> scope = {
+    std::make_pair<double, double>(-1, 1),
+    std::make_pair<double, double>(1, 0) };
+
+  EXPECT_ANY_THROW(simpsonMethod(scope, f1, 10));
+}
+
+TEST(Input_Data_Test, Scope_Interval_Test3) {
+  std::vector<std::pair<double, double>> scope;
+
+  EXPECT_ANY_THROW(simpsonMethod(scope, f1, 10));
+}
+
+TEST(Input_Data_Test, Precision_Test) {
+  std::vector<std::pair<double, double>> scope = 
+  { std::make_pair<double, double>(0, 10) };
+
+  EXPECT_ANY_THROW(simpsonMethod(scope, f1, 0));
+}
+
+TEST(Simpson_Method_Test, Simpson_Test1) {
+  std::vector<std::pair<double, double>> scope = { 
+    std::make_pair<double, double>(0, 10)};
+
+  EXPECT_DOUBLE_EQ(50, simpsonMethod(scope, f1, 10));
+}
+
+TEST(Simpson_Method_Test, Simpson_Test2) {
+  std::vector<std::pair<double, double>> scope = {
+    std::make_pair<double, double>(-5, 5), 
+    std::make_pair<double, double>(-5, 5) };
+
+  EXPECT_DOUBLE_EQ(0, simpsonMethod(scope, f2, 2));
+}
+
+TEST(Simpson_Method_Test, Simpson_Test3) {
+  std::vector<std::pair<double, double>> scope = {
+    std::make_pair<double, double>(0, 8) };
+
+  EXPECT_DOUBLE_EQ(32, simpsonMethod(scope, f2, 16));
+}
+
+TEST(Simpson_Method_Test, Simpson_Test4) {
+  std::vector<std::pair<double, double>> scope = {
+    std::make_pair<double, double>(0, 8),
+    std::make_pair<double, double>(0, 8) };
+
+  EXPECT_DOUBLE_EQ(64, simpsonMethod(scope, f2, 16));
 }
--- modules/task_1/alekhin_d_simpson/simpson.cpp
+++ modules/task_1/alekhin_d_simpson/simpson.cpp
@@ -1,3 +1,51 @@
 // Copyright 2021 Alekhin Denis
 
 #include "../../modules/task_1/alekhin_d_simpson/simpson.h"
+
+double simpsonMethod(std::vector<std::pair<double, double>> scope, 
+  std::function<double(std::vector<double>)> func, 
+  int precision) {
+  
+  if (scope.size() == 0) {
+    throw "Error: scope can't be empty!";
+  }
+  for (size_t i = 0; i < scope.size(); i++) {
+    if (scope[i].first >= scope[i].second) {
+      throw "Error: wrong scope interval!";
+    }
+  }
+  if (precision <= 0) {
+    throw "Error: precision can't be less than 1";
+  }
+
+  int countOfDimentions = scope.size();
+  std::vector<double> h(countOfDimentions);
+  double hComp = 1;
+  for (size_t i = 0; i < countOfDimentions; i++) {
+    h[i] = (scope[i].second - scope[i].first) / precision;
+    hComp *= h[i];
+  }
+
+  double evenSum = 0, oddSum = 0;
+  std::vector<double> point(countOfDimentions);
+  for (size_t i = 1; i < precision; i++) {
+    for (size_t j = 0; j < countOfDimentions; j++) {
+      point[j] = scope[j].first + h[j] * i;
+    }
+    if (i % 2 == 0) {
+      evenSum += func(point);
+    } else {
+      oddSum += func(point);
+    }
+  }
+
+  std::vector<double> a(countOfDimentions), b(countOfDimentions);
+  for (size_t i = 0; i < countOfDimentions; i++) {
+    a[i] = scope[i].first;
+    b[i] = scope[i].second;
+  }
+
+  double result = hComp * (func(a) + func(b) + 2.0 * evenSum + 4.0 * oddSum) / 3.0;
+
+  return result;
+}
