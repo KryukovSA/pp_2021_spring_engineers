04b768427253b19f5b1791a38358a9ae4a270df2
Kryukov.SA <sergey.krykov.2806@gmail.com>
2021-03-14 09:34:51 +0300

    ex

--- modules/task_1/kryukov_s_sparse_compl_mat/kryukov_s_sparse_compl_mat.h
+++ modules/task_1/kryukov_s_sparse_compl_mat/kryukov_s_sparse_compl_mat.h
@@ -1,8 +1,16 @@
 // Copyright 2021 Kryukov Sergey
 #ifndef MODULES_TASK_1_KRYUKOV_S_SPARSE_COMPL_MAT_KRYUKOV_S_SPARSE_COMPL_MAT_H_
 #define MODULES_TASK_1_KRYUKOV_S_SPARSE_COMPL_MAT_KRYUKOV_S_SPARSE_COMPL_MAT_H_
+//формирование в формат хранения CRS
+//функцию умножения двух разреженных матриц
 
 #include <vector>
+#include <complex>
 
+struct crs_mat {
+	std::vector<std::complex<double>> val;
+	std::vector<int> colNum, rowMum;
+
+};
 
 #endif  // MODULES_TASK_1_KRYUKOV_S_SPARSE_COMPL_MAT_KRYUKOV_S_SPARSE_COMPL_MAT_H_
