4c1c3a834bbb8466d961b086bb2a798f1fba9f50
DenisOgnev <denisognev@icloud.com>
2021-03-18 20:32:27 +0300

    Second Attempt

--- modules/task_1/ognev_d_markings_hulls/markings_hulls.cpp
+++ modules/task_1/ognev_d_markings_hulls/markings_hulls.cpp
@@ -196,10 +196,10 @@ int orientTriangle2(int x1, int y1, int x2, int y2, int x3, int y3) {
 }
 
 bool isInside(int x1, int y1, int x2, int y2, int x3, int y3) {
-  if (x2 >= x1 && x2 <= x3 && y2 >= y1 && y2 <= y3 ||
-      x2 >= x3 && x2 <= x1 && y2 >= y1 && y2 <= y3 ||
-      x2 >= x1 && x2 <= x3 && y2 >= y3 && y2 <= y1 ||
-      x2 >= x3 && x2 <= x1 && y2 >= y3 && y2 <= y1) {
+  if ((x2 >= x1 && x2 <= x3 && y2 >= y1 && y2 <= y3) ||
+      (x2 >= x3 && x2 <= x1 && y2 >= y1 && y2 <= y3) ||
+      (x2 >= x1 && x2 <= x3 && y2 >= y3 && y2 <= y1) ||
+      (x2 >= x3 && x2 <= x1 && y2 >= y3 && y2 <= y1)) {
     return true;
   }
   return false;
