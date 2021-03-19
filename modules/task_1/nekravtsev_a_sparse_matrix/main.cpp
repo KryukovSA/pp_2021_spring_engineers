e3901775bdea0d4418a22b51cfd10e2963c93711
Alexey42 <550001@mail.ru>
2021-03-19 20:03:43 +0300

    Алексей Некравцев. Задача 1. Умножение разреженных матриц. Элементы типа double. Формат хранения матрицы – столбцовый (CCS).

---
+++ modules/task_1/nekravtsev_a_sparse_matrix/main.cpp
@@ -0,0 +1,132 @@
+// Copyright 2021 Nekravtsev Alexey
+#include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
+#include "./sparse_matrix.h"
+
+TEST(Seq, test_ordinary_mult) {
+  std::vector<double> A = {
+    1.25, 0, 3.754,
+    0, 0.4, 5.12,
+    9, 0, 0
+  };
+  std::vector<double> B = {
+    1, 0, 3,
+    0, 0, 5,
+    9, 0, 0
+  };
+  std::vector<double> C = MultMatrix(3, A, B);
+
+  std::vector<double> res = { 35.036, 0, 3.75, 46.08, 0, 2, 9, 0, 27 };
+  bool check = true;
+  for (int i = 0; i < res.size(); i++) {
+    if (fabs(C[i] - res[i]) >= std::numeric_limits<double>::epsilon()) {
+      check = false;
+      break;
+    }
+  }
+
+  ASSERT_EQ(true, check);
+}
+
+TEST(Seq, check_results_3x3) {
+  CCS A = createCCS(3, std::vector<double> {
+    1.25, 0, 3.754,
+    0, 0.4, 5.12,
+    9, 0, 0
+  });
+  CCS B = createCCS(3, std::vector<double> {
+    1, 0, 3,
+    0, 0, 5,
+    9, 0, 0
+  });
+  CCS C = MultCCS(TranspSpareMatrix(A), B);
+
+  std::vector<double> res = { 35.036, 46.08, 9, 3.75, 2, 27 };
+  bool check = true;
+  for (int i = 0; i < res.size(); i++) {
+    if (fabs(C.value[i] - res[i]) >= std::numeric_limits<double>::epsilon()) {
+      check = false;
+      break;
+    }
+  }
+
+  ASSERT_EQ(true, check);
+}
+
+TEST(Seq, mult_zero_matr) {
+  CCS A = createCCS(3, std::vector<double>(9, 0));
+  CCS B = createCCS(3, std::vector<double> {
+    1, 0, 3,
+    0, 0, 5,
+    9, 0, 0
+  });
+  CCS C = MultCCS(TranspSpareMatrix(A), B);
+
+  ASSERT_EQ(0, C.value.size());
+}
+
+TEST(Seq, mult_random_matr_3x3) {
+  std::vector<double> a = GenSpareMatrix(3);
+  std::vector<double> b = GenSpareMatrix(3);
+  CCS c = createCCS(3, MultMatrix(3, a, b));
+
+  CCS A = createCCS(3, a);
+  CCS B = createCCS(3, b);
+  CCS C = MultCCS(TranspSpareMatrix(A), B);
+
+  bool check = true;
+  for (int i = 0; i < C.value.size(); i++) {
+    if (fabs(C.value[i] - c.value[i]) >= std::numeric_limits<double>::epsilon()) {
+      check = false;
+      break;
+    }
+  }
+
+  ASSERT_EQ(true, check);
+}
+
+TEST(Seq, mult_random_matr_7x7) {
+  std::vector<double> a = GenSpareMatrix(7);
+  std::vector<double> b = GenSpareMatrix(7);
+  CCS c = createCCS(7, MultMatrix(7, a, b));
+
+  CCS A = createCCS(7, a);
+  CCS B = createCCS(7, b);
+  CCS C = MultCCS(TranspSpareMatrix(A), B);
+
+  bool check = true;
+  for (int i = 0; i < C.value.size(); i++) {
+    if (fabs(C.value[i] - c.value[i]) >= std::numeric_limits<double>::epsilon()) {
+      check = false;
+      break;
+    }
+  }
+
+  ASSERT_EQ(true, check);
+}
+
+TEST(Seq, mult_random_matr_13x13) {
+  std::vector<double> a = GenSpareMatrix(13);
+  std::vector<double> b = GenSpareMatrix(13);
+  CCS c = createCCS(13, MultMatrix(13, a, b));
+
+  CCS A = createCCS(13, a);
+  CCS B = createCCS(13, b);
+  CCS C = MultCCS(TranspSpareMatrix(A), B);
+
+  bool check = true;
+  for (int i = 0; i < C.value.size(); i++) {
+    if (fabs(C.value[i] - c.value[i]) >= std::numeric_limits<double>::epsilon()) {
+      check = false;
+      break;
+    }
+  }
+
+  ASSERT_EQ(true, check);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
