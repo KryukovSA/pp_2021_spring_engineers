3721512bebda70d6c8840ec36919d47ffb5a371c
AnisimovaPV <polinaanis05@mail.ru>
2021-03-22 19:46:28 +0300

    v2

--- modules/task_1/anisimova_p_sparse_matrix/sparsematrix.h
+++ modules/task_1/anisimova_p_sparse_matrix/sparsematrix.h
@@ -4,23 +4,22 @@
 
 #include <vector>
 
-
 class SparseMatrix {
-private:
-    std::vector<double> values;
-    std::vector<int> col_indexes, rows;
-    int num_nzero, size;
+ private:
+  std::vector<double> values;
+  std::vector<int> col_indexes, rows;
+  int num_nzero, size;
 
-public:
-    SparseMatrix(const std::vector<double>& _values,
-        const std::vector<int>& _col_indexes, const std::vector<int>& _rows,
-        int _num_nzero, int _size);
+ public:
+  SparseMatrix(const std::vector<double>& _values,
+               const std::vector<int>& _col_indexes,
+               const std::vector<int>& _rows, int _num_nzero, int _size);
 
-    SparseMatrix(int _size, int _num_nzero,  unsigned int seed = 475);  // Random
-    bool operator==(const SparseMatrix& SM);
+  SparseMatrix(int _size, int _num_nzero, unsigned int seed = 475);  // Random
+  bool operator==(const SparseMatrix& SM);
 
-    SparseMatrix Transpose();
-    SparseMatrix operator*(const SparseMatrix& SM);
+  SparseMatrix Transpose();
+  SparseMatrix operator*(const SparseMatrix& SM);
 };
 
 #endif  // MODULES_TASK_1_ANISIMOVA_P_SPARSEMATRIX_H_
