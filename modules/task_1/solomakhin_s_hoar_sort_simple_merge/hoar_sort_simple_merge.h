396c1612868e94eb171b676ae2d00e5a8f804717
Fess00 <ss_s0l0@mail.ru>
2021-03-23 17:05:18 +0300

    Task_1_fixed_7

--- modules/task_1/solomakhin_s_hoar_sort_simple_merge/hoar_sort_simple_merge.h
+++ modules/task_1/solomakhin_s_hoar_sort_simple_merge/hoar_sort_simple_merge.h
@@ -3,10 +3,9 @@
 #define MODULES_TASK_1_SOLOMAKHIN_S_HOAR_SORT_SIMPLE_MERGE_HOAR_SORT_SIMPLE_MERGE_H_
 
 #include <vector>
-	
+
 std::vector<int> random_gen(int size);
 std::vector<int> hoar_sort(const std::vector<int>& arr, int first, int last);
 std::vector<int> merge(const std::vector<int>& a, const std::vector<int>& b);
-
 
 #endif  // MODULES_TASK_1_SOLOMAKHIN_S_HOAR_SORT_SIMPLE_MERGE_HOAR_SORT_SIMPLE_MERGE_H_
--- modules/task_1/solomakhin_s_hoar_sort_simple_merge/main.cpp
+++ modules/task_1/solomakhin_s_hoar_sort_simple_merge/main.cpp
@@ -81,8 +81,7 @@ TEST(merging, runwithsize1000tosize2000) {
 
 
 
-int main(int argc, char** argv) 
-{
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
