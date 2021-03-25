d53fdb0e044e4a2260064409ae3cc31cb26a2d26
Kryukov.SA <sergey.krykov.2806@gmail.com>
2021-03-25 23:11:36 +0300

    add generate

---
+++ modules/task_2/kryukov_s_sparse_mat_omp/main.cpp
@@ -0,0 +1,94 @@
+// Copyright 2021 Kryukov Sergey
+#include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
+#include <windows.h> 
+#include<iostream>
+#include "./sparsemat_omp.h"
+
+TEST(omp_version, correct_transpose_mat) {
+    int size = 3;
+    std::vector<std::complex<double>> standartMat = {
+        {0, 0}, {0, 2}, {0, 0},
+        {0, 1}, {0, 0}, {0, 0},
+        {0, 0}, {0, 1}, {0, 0}
+    };
+    crs_mat TransponationMat, sparseMat;
+    sparseMat = createSparseMat(size, standartMat);
+    TransponationMat = transposeMatrixGustavson(sparseMat);
+    std::vector<int> rightRowNum = { 0, 1, 3, 3 };
+    ASSERT_EQ(TransponationMat.rowNum, rightRowNum);
+}
+
+TEST(omp_version, correct_transpose_mat2) {
+    int size = 4;
+    std::vector<std::complex<double>> standartMat = {
+        {0, 0}, {0, 2}, {0, 0}, {0, 0},
+        {0, 1}, {0, 0}, {0, 0}, {0, 3},
+        {0, 0}, {0, 1}, {0, 0}, {0, 0},
+        {0, 0}, {0, 0}, {0, 0}, {0, 0}
+    };
+    crs_mat TransponationMat, sparseMat;
+    sparseMat = createSparseMat(size, standartMat);
+    TransponationMat = transposeMatrixGustavson(sparseMat);
+    std::vector<int> rightRowNum = { 0, 1, 3, 3, 4 };
+    ASSERT_EQ(TransponationMat.rowNum, rightRowNum);
+}
+
+TEST(omp_version, check_multiplic) {
+    int size = 2;
+    std::vector<std::complex<double>> standartMat1 = {
+        {0, 0}, {2, 0},
+        {1, 0}, {0, 0}
+    };
+    std::vector<std::complex<double>> standartMat2 = {
+        {0, 0}, {2, 0},
+        {2, 0}, {0, 0}
+    };
+
+    crs_mat SparseMat1, SparseMat2, SparseMatResult;
+    SparseMat1 = createSparseMat(size, standartMat1);
+    SparseMat2 = createSparseMat(size, standartMat2);
+    SparseMatResult = multiplicateMatrix(SparseMat1, SparseMat2);
+    std::vector<std::complex<double>> rightVal = { {4, 0}, {2, 0} };
+    ASSERT_EQ(SparseMatResult.val, rightVal);
+}
+
+TEST(omp_version, check_multiplic2) {
+    int size = 2;
+    std::vector<std::complex<double>> standartMat1 = {
+        {0, 0}, {0, 0},
+        {0, 0}, {0, 0}
+    };
+    std::vector<std::complex<double>> standartMat2 = {
+        {0, 0}, {2, 0},
+        {2, 0}, {0, 0}
+    };
+
+    crs_mat SparseMat1, SparseMat2, SparseMatResult;
+    SparseMat1 = createSparseMat(size, standartMat1);
+    SparseMat2 = createSparseMat(size, standartMat2);
+    SparseMatResult = multiplicateMatrix(SparseMat1, SparseMat2);
+    std::vector<std::complex<double>> rightVal = {};
+    ASSERT_EQ(SparseMatResult.val, rightVal);
+}
+
+
+TEST(omp_version, check_multiplic1000) {
+    int size = 1000;
+
+    crs_mat SparseMat1 = genDiagonalSparseMat(size);
+    crs_mat SparseMat2 = genDiagonalSparseMat(size);
+    std::vector<std::complex<double>> rightRes;
+
+    rightRes = MultipluValues(SparseMat1.val, SparseMat2.val, size);
+
+    crs_mat SparseMatResult = multiplicateMatrix(SparseMat1, SparseMat2);
+    Sleep(2000);
+    EXPECT_EQ(SparseMatResult.val, rightRes);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
