547109c6bace423e8b04f008c25eafcaaceb1eba
Fess00 <ss_s0l0@mail.ru>
2021-03-26 14:01:59 +0300

    Task_1_final

--- modules/task_1/solomakhin_s_hoar_sort_simple_merge/hoar_sort_simple_merge.h
+++ modules/task_1/solomakhin_s_hoar_sort_simple_merge/hoar_sort_simple_merge.h
@@ -4,9 +4,9 @@
 
 #include <vector>
 
-std::vector<double> random_gen(int size);
-std::vector<double> hoar_sort(int left, int right, std::vector <double>& arr);
-//std::vector<int> hoar_sort(std::vector<int>* arr, int first, int last);
-std::vector<double> merge(const std::vector<double>& a, const std::vector<double>& b);
+std::vector<int> random_gen(int size);
+void hoar_sort(int left, int right, std::vector <int>* arr);
+bool checker(std::vector <int>* vec);
+std::vector<int> merge(const std::vector<int>& a, const std::vector<int>& b);
 
 #endif  // MODULES_TASK_1_SOLOMAKHIN_S_HOAR_SORT_SIMPLE_MERGE_HOAR_SORT_SIMPLE_MERGE_H_
--- modules/task_1/solomakhin_s_hoar_sort_simple_merge/main.cpp
+++ modules/task_1/solomakhin_s_hoar_sort_simple_merge/main.cpp
@@ -12,46 +12,42 @@ TEST(randomgen, wrongsize) {
 
 TEST(hoarsort, runwithoutexceptionswithsize10) {
     auto vect = random_gen(10);
-    ASSERT_NO_THROW(hoar_sort(0, 9, vect));
+    ASSERT_NO_THROW(hoar_sort(0, 9, &vect));
 }
 
 TEST(hoarsort, runwithoutexceptionswithsize_100) {
     auto vect = random_gen(100);
-    ASSERT_NO_THROW(hoar_sort(0, 99,vect));
+    ASSERT_NO_THROW(hoar_sort(0, 99, &vect));
 }
 
 TEST(hoarsort, runwithoutexceptionswithsize1000) {
     auto vect = random_gen(1000);
-    ASSERT_NO_THROW(hoar_sort(0, 999, vect));
+    ASSERT_NO_THROW(hoar_sort(0, 999, &vect));
 }
 
 TEST(hoarsort, runwithsize10) {
-    //auto vect = random_gen(10);
-    std::vector<double> vect = { 2,5,4,18,88,44,7,10,22,34 };
-    auto sorted = hoar_sort(0, 9, vect);
-    std::sort(vect.begin(), vect.end());
-    ASSERT_EQ(vect, sorted);
+    auto vect = random_gen(10);
+    hoar_sort(0, 9, &vect);
+    ASSERT_EQ(1, checker(&vect));
 }
 
 TEST(hoarsort, runwithsize100) {
     auto vect = random_gen(100);
-    auto sorted = hoar_sort(0, 99, vect);
-    std::sort(vect.begin(), vect.end());
-    ASSERT_EQ(vect, sorted);
+    hoar_sort(0, 99, &vect);
+    ASSERT_EQ(1, checker(&vect));
 }
 
 TEST(hoarsort, runrandomsize1000) {
     auto vect = random_gen(1000);
-    auto sorted = hoar_sort(0, 999, vect);
-    std::sort(vect.begin(), vect.end());
-    ASSERT_EQ(vect, sorted);
+    hoar_sort(0, 999, &vect);
+    ASSERT_EQ(1, checker(&vect));
 }
 
 TEST(merging, runwithsize10tosize20) {
     auto vect_a = random_gen(10);
     auto vect_b = random_gen(10);
-    vect_a = hoar_sort( 0, 9, vect_a);
-    vect_b = hoar_sort(0, 9, vect_b);
+    hoar_sort(0, 9, &vect_a);
+    hoar_sort(0, 9, &vect_b);
     auto merged = merge(vect_a, vect_b);
     for (int i = 0; i < static_cast<int>(vect_a.size()) - 1; ++i) {
         ASSERT_TRUE(merged[i] <= merged[i + 1]);
@@ -61,8 +57,8 @@ TEST(merging, runwithsize10tosize20) {
 TEST(merging, runwithsize100tosize200) {
     auto vect_a = random_gen(100);
     auto vect_b = random_gen(100);
-    vect_a = hoar_sort(0, 99, vect_a);
-    vect_b = hoar_sort(0, 99, vect_b);
+    hoar_sort(0, 99, &vect_a);
+    hoar_sort(0, 99, &vect_b);
     auto merged = merge(vect_a, vect_b);
     for (int i = 0; i < static_cast<int>(vect_a.size()) - 1; ++i) {
         ASSERT_TRUE(merged[i] <= merged[i + 1]);
@@ -72,8 +68,8 @@ TEST(merging, runwithsize100tosize200) {
 TEST(merging, runwithsize1000tosize2000) {
     auto vect_a = random_gen(1000);
     auto vect_b = random_gen(1000);
-    vect_a = hoar_sort(0, 999, vect_a);
-    vect_b = hoar_sort(0, 999, vect_b);
+    hoar_sort(0, 999, &vect_a);
+    hoar_sort(0, 999, &vect_b);
     auto merged = merge(vect_a, vect_b);
     for (int i = 0; i < static_cast<int>(vect_a.size()) - 1; ++i) {
         ASSERT_TRUE(merged[i] <= merged[i + 1]);
