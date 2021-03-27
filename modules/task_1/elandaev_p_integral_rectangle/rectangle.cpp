8ccc9b687d57c77b24a5810a7a10ce46fa153ac7
elandaev <p.elandaev@gmail.com>
2021-03-28 00:24:55 +0300

    CodeStyle

--- modules/task_1/elandaev_p_integral_rectangle/rectangle.cpp
+++ modules/task_1/elandaev_p_integral_rectangle/rectangle.cpp
@@ -2,17 +2,17 @@
 
 #include "../../modules/task_1/elandaev_p_integral_rectangle/rectangle.h"
 
-double RectangleIntegral(double start, double end, std::function<double(double)> f, double step){
-    if(end < start)
+double RectangleIntegral(double start, double end, std::function<double(double)> f, double step) {
+    if (end < start)
         throw "wrong interval";
-    if(step <= 0)
+    if (step <= 0)
         throw "wrong splitting: smallest";
-    if(step > end - start)
+    if (step > end - start)
         throw "step is biggest";
-    int countstep = (int)(end - start) / step;
+    int countstep = static_cast<int>(end - start) / step;
     double x;
     double sum = 0.0;
-    for(double i = 0.0; i < countstep; i ++){
+    for (double i = 0.0; i < countstep; i ++) {
         x = start + i * step;
         sum += f(x);
     }
