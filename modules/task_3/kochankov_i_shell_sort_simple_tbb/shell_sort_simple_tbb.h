aa0e3fb19746ce74c30c483f50c6040de2a14783
ilya kochankov <ikochankov@neuro.net>
2021-04-02 09:14:02 +0300

    delete omp

--- modules/task_3/kochankov_i_shell_sort_simple_tbb/shell_sort_simple_tbb.h
+++ modules/task_3/kochankov_i_shell_sort_simple_tbb/shell_sort_simple_tbb.h
@@ -7,7 +7,6 @@
 
 std::vector<double> getRandomVector(int sz);
 std::vector<double> shell_sort(const std::vector<double>& vec);
-std::vector<double> shell_sort_omp(const std::vector<double>& vec, int num_threads = 4);
 std::vector<double> shell_sort_tbb(const std::vector<double>& vec, int num_threads = 4);
 std::vector<double> merge(const std::vector<double>& a, const std::vector<double>& b);
 
